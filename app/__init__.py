15209c2e8149cfb4296c4b10a90c5931f6359eb4
toddcmz <toddcmz@gmail.com>
2023-05-09 22:07:39 -0600

    sign up and sql data working

--- app/__init__.py
+++ app/__init__.py
@@ -1,6 +1,12 @@
 from flask import Flask
+from config import Config
+from flask_sqlalchemy import SQLAlchemy
+from flask_migrate import Migrate
 
 app = Flask(__name__)
+app.config.from_object(Config)
+db = SQLAlchemy(app)
+migrate = Migrate(app, db)
 
 from app.blueprints.auth import bp as auth_bp
 app.register_blueprint(auth_bp)
@@ -8,3 +14,5 @@ from app.blueprints.main import bp as main_bp
 app.register_blueprint(main_bp)
 from app.blueprints.mybooks import bp as mybooks_bp
 app.register_blueprint(mybooks_bp)
+
+from app import models
--- app/blueprints/auth/routes.py
+++ app/blueprints/auth/routes.py
@@ -1,10 +1,28 @@
-from flask import render_template
+from flask import render_template, flash, redirect, url_for
 from . import bp
+from app.forms import SignupForm
+from app.models import User
+from app import db
 
 @bp.route('/signin')
 def signin():
     return render_template('signin.jinja', title='Sign In')
 
-@bp.route('/signup')
+@bp.route('/signup', methods=['GET', 'POST'])
 def signup():
-    return render_template('signup.jinja', title='Sign Up')
+    form = SignupForm()
+    if form.validate_on_submit():
+            username = form.username.data
+            email = form.email.data
+            checkUser = User.query.filter_by(username=username).first()
+            checkEmail = User.query.filter_by(email=email).first()
+            if not checkUser and not checkEmail:
+                thisUser = User(username=username, email=email, password=form.password.data)
+                thisUser.commit()
+                flash(f"Username '{username}' submitted a sign up request")
+                return redirect(url_for('main.home'))
+            elif checkUser:
+                flash(f'{username} already taken, try again')
+            else:
+                flash(f'{email} already taken, try again')
+    return render_template('signup.jinja', title='Sign Up', form=form)
